8516968c2e0b7b28f90f39e2a7db0f287a3d2807
salmanca <mohammedsalmanfaris.c.a@gmail.com>
2022-05-30 19:34:12 +0530

    Added the feature for creating the tags in recipe

--- app/recipe/tests/test_tag_api.py
+++ app/recipe/tests/test_tag_api.py
@@ -52,3 +52,19 @@ class PrivetApiTests(TestCase):
         self.assertEqual(res.status_code, status.HTTP_200_OK)
         self.assertEqual(len(res.data), 1)
         self.assertEqual(res.data[0]['name'], tag.name)
+
+    def test_create_tag_successful(self):
+        payload = {
+            'name' : 'test tag',
+        }
+        res = self.client.post(TAG_URL, payload)
+
+        tag = Tag.objects.filter(user=self.user, name=payload['name']).exists()
+        self.assertTrue(tag)
+
+    def test_create_tag_invalid(self):
+        payload = {'name' : ''}
+        res = self.client.post(TAG_URL, payload)
+
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+        
--- app/recipe/views.py
+++ app/recipe/views.py
@@ -5,7 +5,9 @@ from rest_framework.permissions import IsAuthenticated
 from core.models import Tag
 from . import serializers
 
-class TagViewSet(viewsets.GenericViewSet, mixins.ListModelMixin):
+class TagViewSet(viewsets.GenericViewSet, 
+                 mixins.ListModelMixin,
+                 mixins.CreateModelMixin):
     authentication_classes = (TokenAuthentication, )
     permission_classes = (IsAuthenticated,)
     queryset = Tag.objects.all()
@@ -13,3 +15,6 @@ class TagViewSet(viewsets.GenericViewSet, mixins.ListModelMixin):
 
     def get_queryset(self):
         return self.queryset.filter(user=self.request.user)
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)

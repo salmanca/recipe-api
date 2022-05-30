1a6cdb68c7473ac743c5ffa1a3a5fc0bc01df638
salmanca <mohammedsalmanfaris.c.a@gmail.com>
2022-05-30 19:16:48 +0530

    Added recipe app, tests for this app and api views for the tags

---
+++ app/recipe/tests/test_tag_api.py
@@ -0,0 +1,54 @@
+from django.urls import reverse
+from django.contrib.auth import get_user_model
+from django.test import TestCase
+from rest_framework import status
+from rest_framework.test import APIClient
+from core.models import Tag
+from recipe.serializers import TagSerializer
+
+TAG_URL = reverse('recipe:tag-list')
+
+class PublicApiTests(TestCase):
+    def setUp(self):
+        self.client = APIClient()
+
+    def test_login_required(self):
+        res = self.client.get(TAG_URL)
+
+        self.assertEqual(res.status_code, status.HTTP_401_UNAUTHORIZED)
+
+
+class PrivetApiTests(TestCase):
+    def setUp(self):
+        email = 'test@gmail.com'
+        password = 'test123'
+        self.user = get_user_model().objects.create_user(
+            email,
+            password
+        )
+        self.client = APIClient()
+        self.client.force_authenticate(self.user)
+
+    def test_retrive_tags(self):
+        Tag.objects.create(user = self.user, name = 'Vegan')
+        Tag.objects.create(user = self.user, name = 'Desert')
+        
+        res = self.client.get(TAG_URL)
+        tags = Tag.objects.all().order_by('name')
+        serializer = TagSerializer(tags, many=True)
+        
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.assertEqual(res.data, serializer.data)
+
+    def test_tags_limited_user(self):
+        user2 = get_user_model().objects.create_user(
+            email = 'test1@gmail.com',
+            password = 'test123'
+        )
+        Tag.objects.create(user = user2, name = 'Biriyani')
+        tag = Tag.objects.create(user = self.user, name = 'Vegan')
+        res = self.client.get(TAG_URL)
+
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(res.data), 1)
+        self.assertEqual(res.data[0]['name'], tag.name)
